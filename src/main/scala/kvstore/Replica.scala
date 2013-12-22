1a53760fe9ca054f06a2b70fe00dfccef7b17070
prystupa <eugene.prystupa@gmail.com>
2013-12-22 17:06:26 -0500

    pass Step1_PrimarySpec

--- src/main/scala/kvstore/Replica.scala
+++ src/main/scala/kvstore/Replica.scala
@@ -1,11 +1,11 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor.{OneForOneStrategy, Props, ActorRef, Actor}
 import kvstore.Arbiter._
 import scala.collection.immutable.Queue
 import akka.actor.SupervisorStrategy.Restart
 import scala.annotation.tailrec
-import akka.pattern.{ ask, pipe }
+import akka.pattern.{ask, pipe}
 import akka.actor.Terminated
 import scala.concurrent.duration._
 import akka.actor.PoisonPill
@@ -14,23 +14,32 @@ import akka.actor.SupervisorStrategy
 import akka.util.Timeout
 
 object Replica {
+
   sealed trait Operation {
     def key: String
+
     def id: Long
   }
+
   case class Insert(key: String, value: String, id: Long) extends Operation
+
   case class Remove(key: String, id: Long) extends Operation
+
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
+
   case class OperationAck(id: Long) extends OperationReply
+
   case class OperationFailed(id: Long) extends OperationReply
+
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
+
   import Replica._
   import Replicator._
   import Persistence._
@@ -39,21 +48,39 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  override def preStart(): Unit = {
+    arbiter ! Join
+  }
+
   def receive = {
-    case JoinedPrimary   => context.become(leader)
+    case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
-    case _ =>
+    case Insert(key, value, id) => {
+      kv = kv updated(key, value)
+      sender ! OperationAck(id)
+    }
+
+    case Remove(key, id) => {
+      kv = kv - key
+      sender ! OperationAck(id)
+    }
+
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }
+
+    case _ => ???
   }
 
   /* TODO Behavior for the replica role. */
